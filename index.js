e8a44ff47fdd2095123f158cae0affed55cc00b6
Joseph Abreau <joseph.abreau@gmail.com>
2023-10-02 19:06:09 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const main = document.getElementById("main");
+main.remove();
+
+const newHeader = document.createElement("H1");
+newHeader.id = "victory";
+newHeader.textContent = "Joey is the champion";
+document.body.append(newHeader);
